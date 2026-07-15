c1f5c9bef778257a17b2ba0a7357ed6dab94c1ec
Gabriel Duraye <gabriel.duraye@proton.me>
2026-07-15 08:02:48 +0100

    fix:add alt networking config

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["neuromancers.org.uk"])
+ALLOWED_HOSTS = [
+    *env.list("DJANGO_ALLOWED_HOSTS", default=["neuromancers.org.uk"]),
+    env("HETZNER_SSH_HOST", default="127.0.0.1"),
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
