15a6c6319d268b76168f28af615c45ca97ca25c4
Gabriel Duraye <gabriel.duraye@proton.me>
2026-07-15 18:41:21 +0100

    fix:amend sentry logging

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,6 +5,7 @@ import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa: F403
@@ -217,11 +218,10 @@ LOGGING = {
 # ------------------------------------------------------------------------------
 SENTRY_DSN = env("SENTRY_DSN")
 SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
-
+ignore_logger("django.security.DisallowedHost")
 sentry_logging = LoggingIntegration(
     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
     event_level=logging.ERROR,  # Send errors as events
-    ignored_loggers=["django.security.DisallowedHost"],
 )
 integrations = [
     sentry_logging,
